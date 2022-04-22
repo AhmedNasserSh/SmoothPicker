7bf11206fffd8866b81ca77a48db655e1cf6b622
Pierre Marandon <pierre@phylica.fr>
2022-04-22 12:31:30 +0200

    Version and indentation.

--- SmoothPicker/Classes/SmoothPickerConfiguration.swift
+++ SmoothPicker/Classes/SmoothPickerConfiguration.swift
@@ -8,21 +8,30 @@
 
 import Foundation
 import UIKit
-public enum SelectionStyle {
+
+public enum SelectionStyle
+{
     case scale
     case colored
 }
-public class SmoothPickerConfiguration {
-    internal static  var selectionStyle:SelectionStyle?
+
+public class SmoothPickerConfiguration
+{
+    internal static var selectionStyle: SelectionStyle?
     internal static var selectedColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     internal static var dimmedColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-    public static func setSelectionStyle(selectionStyle:SelectionStyle) {
-        if self.selectionStyle == nil {
+
+    public static func setSelectionStyle(selectionStyle: SelectionStyle)
+    {
+        if self.selectionStyle == nil
+        {
             self.selectionStyle = selectionStyle
         }
     }
-   public  static func setColors(selectedColor:UIColor,dimmedColor:UIColor) {
-        self.selectedColor  = selectedColor
+
+    public static func setColors(selectedColor: UIColor, dimmedColor: UIColor)
+    {
+        self.selectedColor = selectedColor
         self.dimmedColor = dimmedColor
     }
 }
